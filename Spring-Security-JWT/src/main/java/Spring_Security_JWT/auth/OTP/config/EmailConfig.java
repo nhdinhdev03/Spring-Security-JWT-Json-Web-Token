1df999b91e8d111153788dc81eb9a10ed7828812
nhdinhdev03 <nhdinh.dev03@gmail.com>
2025-08-23 21:43:16 +0700

    email

--- Spring-Security-JWT/src/main/java/Spring_Security_JWT/auth/OTP/config/EmailConfig.java
+++ Spring-Security-JWT/src/main/java/Spring_Security_JWT/auth/OTP/config/EmailConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+//confih email
 @Configuration
 public class EmailConfig {
   @Value("${spring.mail.host}")
